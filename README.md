dd06b1b4207ef9facb857df5f85a800d73666359
arvgg <arvgentilucci@gmail.com>
2015-07-29 13:28:45 -0400

    remove all

--- README.md
+++
@@ -1,2 +0,0 @@
-# codeeval
-Codeeval Practice Problems
--- codeeval/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-test_cases = open(sys.argv[1], 'r')
-for test in test_cases:
-    test = test.rstrip()
-    if len(test) > 55:
-        test = test[:40].rsplit(' ', 1)[0] + '... <Read More>'
-
-    print test
-
